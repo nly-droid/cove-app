ca8385c905bacf427c0ee1534dce3d9d4916f12a
Nhi Ly <nh915648@dal.ca>
2021-01-12 23:25:37 +0700

    Initial commit

---
+++ desktop/src/com/github/nlydroid/coveapp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.nlydroid.coveapp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.nlydroid.coveapp.CoveApplication;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoveApplication(), config);
+	}
+}
